a1826346d4c30be28d6d038e172cb3ca33ea7d1b
Alex Singh <alex@alexsingh.com>
2019-06-19 07:55:45 -0300

    Updated Geographical Representation styles

--- src/components/GeoRep.js
+++ src/components/GeoRep.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import Title from "../components/Title.js"
 
 
 // Geographic Representation
@@ -7,22 +8,78 @@ import styled from "styled-components"
 const GeographyContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, 240px);
-  justify-items: center;
+  justify-content: center;
+  min-height: 50vh;
 `
 
 const GeographyObject = styled.div`
   display: flex;
   flex-direction: column;
   justify-items: around;
-  align-items: center;
+  align-items: start;
   font-size: 2rem;
+`
+
+const CountryListContainer = styled.ul`
+  font-size: 1.25rem;
+  list-style: none;
+  padding-left: 0px;
+  line-height: 160%;
+`
+
+const CountryListItem = styled.li`
+
 `
 
 
 export default () => (
   <GeographyContainer>
     <GeographyObject>
-      Geography Section is a WIP.
+      <Title>North America</Title>
+        <CountryListContainer>
+          <CountryListItem>United States</CountryListItem>
+          <CountryListItem>Canada</CountryListItem>
+        </CountryListContainer>
+    </GeographyObject>
+    <GeographyObject>
+      <Title>Africa</Title>
+      <CountryListContainer>
+        <CountryListItem>Nigeria</CountryListItem>
+        <CountryListItem>South Africa</CountryListItem>
+        <CountryListItem>Kenya</CountryListItem>
+        <CountryListItem>Ethiopia</CountryListItem>
+        <CountryListItem>Cameroon</CountryListItem>
+      </CountryListContainer>
+    </GeographyObject>
+    <GeographyObject>
+      <Title>Europe</Title>
+      <CountryListContainer>
+        <CountryListItem>Nigeria</CountryListItem>
+        <CountryListItem>South Africa</CountryListItem>
+        <CountryListItem>Kenya</CountryListItem>
+        <CountryListItem>Ethiopia</CountryListItem>
+        <CountryListItem>Cameroon</CountryListItem>
+      </CountryListContainer>
+    </GeographyObject>
+    <GeographyObject>
+      <Title>Asia</Title>
+      <CountryListContainer>
+        <CountryListItem>Nigeria</CountryListItem>
+        <CountryListItem>South Africa</CountryListItem>
+        <CountryListItem>Kenya</CountryListItem>
+        <CountryListItem>Ethiopia</CountryListItem>
+        <CountryListItem>Cameroon</CountryListItem>
+      </CountryListContainer>
+    </GeographyObject>
+    <GeographyObject>
+      <Title>Oceania</Title>
+      <CountryListContainer>
+        <CountryListItem>Nigeria</CountryListItem>
+        <CountryListItem>South Africa</CountryListItem>
+        <CountryListItem>Kenya</CountryListItem>
+        <CountryListItem>Ethiopia</CountryListItem>
+        <CountryListItem>Cameroon</CountryListItem>
+      </CountryListContainer>
     </GeographyObject>
   </GeographyContainer>
 );
