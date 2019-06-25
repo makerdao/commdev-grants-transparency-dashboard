38b77c74192371bb5612bc5bf097754172f08723
Alex Singh <alex@alexsingh.com>
2019-06-25 17:19:45 -0300

    SectionTitle font size reduced, uppercase transform applied

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -1,7 +1,8 @@
 import styled from "styled-components"
 
-const SectionTitle = styled.h5`
+const SectionTitle = styled.h6`
 grid-column: 1 / -1;
+text-transform: uppercase;
 color: #1AAB9B;
 justify-self: center;
 `
