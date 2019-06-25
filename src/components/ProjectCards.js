5d0944403fc4c9e13834b6b0cd930ff89c7ba7a8
Alex Singh <alex@alexsingh.com>
2019-06-25 18:10:21 -0300

    Updated ProjectCard design

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import {SectionTitle} from "../components/SectionTitle.js"
 import {data} from "../utils.js"
 
 
@@ -9,56 +10,76 @@ const ProjectCardWrapper = styled.section`
 display: grid;
 grid-template-columns: repeat(auto-fit, 300px);
 justify-items: space-around;
-grid-gap: 3rem;
+grid-gap: 4rem;
 justify-content: space-around;
+padding: 4rem 0rem 4rem 0rem;
 `
 
 
 const ProjectCard = styled.div`
 display: grid;
-grid-template-columns: 1fr 1fr 1fr;
-grid-template-rows: 1fr 1fr 5fr 1fr;
+grid-template-columns: 1fr 1fr 1fr 1fr;
+grid-template-rows: 1fr 2fr 1fr 1fr;
 width: 300px;
-height: 240px;
+height: 280px;
+margin: 2rem 0rem 2rem 0rem;
 flex-direction: column;
 align-items: flex-start;
 background: hsla(255,255,255,0.4);
+border: 1px solid #aaa;
+padding: 1rem 1rem 0.15rem 1rem;
 `
 
 
-const ProjectTitle = styled.h6`
+const ProjectTitle = styled.h5`
 grid-column: 1 / -1;
 grid-row: 1;
 color: #231536;
 margin:0;
-text-transform: uppercase;
 text-overflow: ellipsis;
 overflow: hidden;
 white-space: nowrap;
 `
 
-const ProjectStatus = styled.h6`
+const Row = styled.div`
 grid-column: 1 / -1;
-grid-row: 2;
-color: #ccc;
-margin-top: 1rem;
+display: grid;
+grid-template-columns: repeat(2, 1fr);
+
+`
+
+const ProjectStatus = styled.h6`
+grid-column: 1 / 2;
+grid-row: 4;
+color: #444;
+text-transform: uppercase;
+`
+
+const ProjectCategory = styled.h6`
+grid-column: 1 / 3;
+grid-row: 3;
+color: #444;
 `
 
 
 const ProjectDescription = styled.p`
 grid-column: 1 / -1;
-grid-row: 3;
-color: #aaa;
+grid-row: 2;
+color: #444;
+max-height: 140px;
 overflow: hidden;
 `
 
 const ButtonLearnMore = styled.a`
-grid-column: 1 / 3;
+grid-column: 3 / -1;
 grid-row: 4;
-border: 1px solid #231536;
+align-self: center;
+justify-self: flex-end;
+border: 1px solid #aaa;
 border-radius: 0.125rem;
-padding: 0.875rem 1.5rem 0.875rem 1.5rem;
-font-size: 1rem;
+padding: 0.875rem 0.875rem 0.875rem 0.875rem;
+max-width: 96px;
+font-size: 1.15rem;
 text-align: center;
 background: none;
 text-decoration: none;
@@ -68,13 +89,15 @@ text-decoration: none;
 
 export default () => (
   <ProjectCardWrapper >
+  <SectionTitle>Project Index</SectionTitle>
   {
     Object.values(data.acceptedProjects).map( project => (
       <ProjectCard key={project.name}>
           <ProjectTitle>{project.name}</ProjectTitle>
+          <ProjectDescription>{project.description}</ProjectDescription>
           <ProjectStatus>{project.status}</ProjectStatus>
-        <ProjectDescription>{project.description}</ProjectDescription>
-        <ButtonLearnMore href={project.learnMoreLink} target="_blank" rel="noopener noreferrer"> Learn More</ButtonLearnMore>
+
+          <ButtonLearnMore href={project.learnMoreLink} target="_blank" rel="noopener noreferrer"> Learn More</ButtonLearnMore>
       </ProjectCard>
     ))
   }
