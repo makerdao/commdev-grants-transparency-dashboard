fbae7eed9866417a537249f427c07924fb448902
Alex Singh <alex@alexsingh.com>
2019-06-26 18:01:54 -0300

    Updated Footer styling, structure to prep for adding responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,25 @@
 import React from "react"
 import styled from "styled-components"
+import {SectionWrapper} from "../components/SectionWrapper.js"
 
-const FooterWrapper = styled.section`
+const FooterRow = styled.section`
+grid-column: 1 / -1;
 display: grid;
-grid-template-columns: repeat(12, 1fr);
-grid-template-rows: auto;
-padding: 6rem 0rem 6rem 0rem;
+grid-template-columns: repeat(auto-fit, 360px);
+grid-row-gap: 2rem;
+justify-content: space-between;
+padding: 3rem 4rem 3rem 4rem;
 `
 
 const FooterTitle = styled.h4`
-grid-row: 1 / 2;
 grid-column: 1 / -1;
 justify-self: center;
 align-self: center;
+font-weight: 300;
 color: #1AAB9B;
 `
 
 const FooterCardContainer = styled.div`
-grid-row: 2;
 grid-column: 1 / -1;
 grid-gap: 3rem;
 display: grid;
@@ -28,14 +30,12 @@ padding: 6rem 0rem 6rem 0rem;
 
 const FooterCardCTA = styled.div`
 display: grid;
-grid-row: 3;
 max-width: 400px;
 grid-template-columns: 48px repeat(2, 1fr);
 grid-template-rows: 48px 1fr;
 `
 
 const FooterCard = styled.div`
-/* grid-row: 1; */
 display: grid;
 max-width: 400px;
 grid-template-columns: 48px repeat(2, 1fr);
@@ -43,7 +43,6 @@ grid-template-rows: 48px 1fr;
 `
 
 const FooterCardNum = styled.h5`
-grid-row:1;
 grid-column: 1;
 justify-self: center;
 align-self: center;
@@ -52,7 +51,7 @@ margin:0px;
 `
 
 const FooterCardTitle = styled.h5`
-grid-row: 1;
+
 grid-column: 2 / -1;
 align-self: center;
 color: #231536;
@@ -60,109 +59,118 @@ margin:0px;
 `
 
 const FooterCardDesc = styled.p`
-grid-row: 2;
 grid-column: 2 / -1;
 color: #48495F;
 margin:0px;
+padding: 1rem 0rem 1rem 0rem;
+line-height: 140%;
 `
 
 const CallToActionContainer = styled.div`
-grid-row: 3;
-grid-column: 3 / -1;
+grid-column: 1 / -1;
 display: grid;
-grid-template-columns: repeat(6, 1fr);
+grid-template-columns: minmax(300px, 540px);
 grid-gap: 2rem;
 justify-content: center;
 align-items: center;
+justify-self: flex-start;
+border: 1px solid #ddd;
 `
 
-const CallToActionLinks = styled.a`
+const ButtonContainer = styled.div`
+grid-column: 1  -1;
+display: grid;
+justify-items: center;
+`
+
+const CallToActionLink = styled.a`
+width: 100%;
+grid-row: 2;
 display: flex;
 height: 64px;
 border: 1px solid #1AAB9B;
-border-radius: 2px;
 background: #1AAB9B;
 color: #fafafa;
-font-size: 1.5rem;
+font-size: 1.25rem;
 justify-content: center;
 align-items: center;
-padding: 0rem 4rem 0rem 4rem;
 `
 
 const CallToActionFAQ = styled.a`
+width: 100%;
+grid-row: 2;
 display: flex;
 height: 64px;
 border: 1px solid #1AAB9B;
-border-radius: 2px;
 color: #1AAB9B;
-background: #fafafa;
-font-size: 1.5rem;
+background: none;
+font-size: 1.25rem;
 justify-content: center;
 align-items: center;
-padding: 0rem 4rem 0rem 4rem;
 `
 
 export default () => (
-  <FooterWrapper>
+  <SectionWrapper name="Footer">
     <FooterTitle>Apply for a grant</FooterTitle>
-    <FooterCardContainer>
-      <FooterCard>
-      <FooterCardNum>
-        1
-      </FooterCardNum>
-      <FooterCardTitle>
-        Select a category
-      </FooterCardTitle>
-      <FooterCardDesc>
-        Choose the funding category that most interests you, or best fits your existing project
-      </FooterCardDesc>
-      </FooterCard>
+    <FooterRow>
+        <FooterCard>
+        <FooterCardNum>
+          1
+        </FooterCardNum>
+        <FooterCardTitle>
+          Select a category
+        </FooterCardTitle>
+        <FooterCardDesc>
+          Choose the funding category that most interests you, or best fits your existing project
+        </FooterCardDesc>
+        </FooterCard>
 
-      <FooterCard>
-      <FooterCardNum>
-        2
-      </FooterCardNum>
-      <FooterCardTitle>
-        Submit your proposal
-      </FooterCardTitle>
-      <FooterCardDesc>
-        Answer a handful of simple questions so we can learn about your project .
-      </FooterCardDesc>
-      </FooterCard>
+        <FooterCard>
+        <FooterCardNum>
+          2
+        </FooterCardNum>
+        <FooterCardTitle>
+          Submit your proposal
+        </FooterCardTitle>
+        <FooterCardDesc>
+          Answer a handful of simple questions so we can learn about your project .
+        </FooterCardDesc>
+        </FooterCard>
 
-      <FooterCard>
-      <FooterCardNum>
-        3
-      </FooterCardNum>
-      <FooterCardTitle>
-        Build A Prototype
-      </FooterCardTitle>
-      <FooterCardDesc>
-        Get coding! Grants are paid intermittently as you meet your milestones.
-      </FooterCardDesc>
-      </FooterCard>
-      <FooterCardCTA>
-      <FooterCardNum>
-        →
-      </FooterCardNum>
-      <FooterCardTitle>
-        Ready to Apply?
-      </FooterCardTitle>
-      <FooterCardDesc>
-        All you need is 10 minutes and a little enthusiasm for contributing to the future of crypto.
-      </FooterCardDesc>
-      </FooterCardCTA>
-
+        <FooterCard>
+        <FooterCardNum>
+          3
+        </FooterCardNum>
+        <FooterCardTitle>
+          Build A Prototype
+        </FooterCardTitle>
+        <FooterCardDesc>
+          Get coding! Grants are paid intermittently as you meet your milestones.
+        </FooterCardDesc>
+        </FooterCard>
+      </FooterRow>
+      <FooterRow>
       <CallToActionContainer>
-      <CallToActionLinks>
-        Apply
-      </CallToActionLinks>
-      <CallToActionFAQ>
-        FAQ
-      </CallToActionFAQ>
+        <FooterCardCTA>
+        <FooterCardNum>
+          →
+        </FooterCardNum>
+        <FooterCardTitle>
+          Ready to Apply?
+        </FooterCardTitle>
+        <FooterCardDesc>
+          All you need is 10 minutes and a little enthusiasm for contributing to the future of crypto.
+        </FooterCardDesc>
+        </FooterCardCTA>
+        <ButtonContainer>
+        <CallToActionLink>
+          Apply
+        </CallToActionLink>
+        <CallToActionFAQ>
+          FAQ
+        </CallToActionFAQ>
+        </ButtonContainer>
       </CallToActionContainer>
-    </FooterCardContainer>
-
-
-  </FooterWrapper>
+    </FooterRow>
+  </SectionWrapper>
 );
