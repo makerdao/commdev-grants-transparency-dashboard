89cc1a2bc81e2a23a7a27ad21cf8a669442ad8fa
Alex Singh <alex@alexsingh.com>
2019-06-28 08:08:07 -0300

    Removed unnecessary conditional props

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -4,9 +4,9 @@ const PrimaryButton = styled.button`
 padding: 0.5rem 0.875rem 0.5rem 0.875rem;
 color: #231536;
 font-size: 1.25rem;
-border: ${props => props.active ? '1px solid #B6EDE7' : '1px solid #ddd'};
+border: 1px solid #ddd;
 border-radius: 2px;
-background: ${props => props.active ? '#B6EDE7' : 'none'};
+background: none;
 transition: 0.2s ease-in-out;
 outline: none;
 
