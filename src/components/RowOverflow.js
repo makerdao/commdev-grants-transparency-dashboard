37791838e68b604305aab5ddbfe1d8610700341f
Alex Singh <alex@alexsingh.com>
2019-07-07 12:39:48 -0300

    Broke out row for overflowing content (ProjectStatus/Countries)

---
+++ src/components/RowOverflow.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const RowOverflow = styled.div`
+grid-column: 2 / -1;
+display: flex;
+flex-direction: flex-row;
+flex-wrap: nowrap;
+overflow-x: auto;
+justify-content: flex-start;
+background-color: hsla(38, 76%, 71%, 0.1);
+`
+
+export {RowOverflow}
