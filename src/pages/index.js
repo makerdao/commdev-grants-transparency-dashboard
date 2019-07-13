ef21142d9f3379161b989218d594c344994cb5fb
Alex Singh <alex@alexsingh.com>
2019-07-13 07:48:48 -0300

    Removed now redundant TopWrapper component

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,12 @@ import PrimaryStats from "../components/PrimaryStats"
 import {ProjectCategories} from "../components/ProjectCategories"
 import Footer from "../components/Footer"
 
-const TopWrapper = styled.section`
-background: linear-gradient(180deg, rgba(182, 237, 231, 0.38) 0%, rgba(253, 193, 52, 0.15) 100%);
-/* background: linear-gradient(180deg, rgba(182, 237, 231, 0.38) 50%, rgba(253, 193, 52, 0) 100%); */
-width: 100%;
-`
 
 export default () => (
   <React.Fragment>
-    <TopWrapper>
       <Header />
       <PrimaryStats />
-    </TopWrapper>
       <ProjectCategories />
-    <Footer />
-
+      <Footer />
   </React.Fragment>
 )
