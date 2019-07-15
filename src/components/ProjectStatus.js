99212a2fb19095fe6cdf591b4452ededa950fa26
Alex Singh <alex@alexsingh.com>
2019-07-15 16:37:06 -0300

    Made b/g gradient seamless

--- src/components/ProjectStatus.js
+++ src/components/ProjectStatus.js
@@ -1,6 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import {RowOverflow} from "../components/RowOverflow.js"
+import {device} from "../mediaqueries/device.js"
 import {data, pieData} from "../utils.js"
 import {pStatus} from '../../static/data/dataformat.js'
 import {Pie} from 'react-chartjs-2'
@@ -20,6 +21,12 @@ justify-content: flex-start;
 align-items: center;
 background: linear-gradient(hsla(168, 50%, 90%, 1), hsla(168, 50%, 86%, 1));
 min-width: 160px;
+
+
+@media ${device.tablet} {
+  /* Changes the orientation (to 90degrees) of the background gradient to make it blend once this element changes from flex-direction: row to flex-direction: column */
+  background: linear-gradient(90deg,hsla(168, 50%, 90%, 1), hsla(168, 50%, 86%, 1));
+}
 `
 
 const ProjectStatusChart = styled.div`
