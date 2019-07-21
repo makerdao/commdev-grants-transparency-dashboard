9a8c6f9c27c8d145bd51be007c1f0eb665967c94
Alex Singh <alex@alexsingh.com>
2019-07-21 18:17:17 -0300

    Added semicolon

--- src/components/SectionWrapper.js
+++ src/components/SectionWrapper.js
@@ -8,7 +8,7 @@ grid-template-columns: repeat(auto-fit, 300px);
 grid-gap: 0rem;
 justify-content: space-around;
 padding: 2rem 1rem 4rem 1rem;
-background: ${props => props.backgroundColor}
+background: ${props => props.backgroundColor};
 `
 
 export {SectionWrapper}
