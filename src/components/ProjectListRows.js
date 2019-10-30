c7f7c9030511721eaff7424e7e865d4ffe1858c1
Alex Singh <alex@alexsingh.com>
2019-10-30 19:34:05 -0300

    Updated 'location' with 'country' per JSON object change

--- src/components/ProjectListRows.js
+++ src/components/ProjectListRows.js
@@ -1,190 +1,178 @@
 import React from "react"
 import styled from "styled-components"
 import projectListRowStyles from "./projectlistrows.module.css"
-import {device} from "../mediaqueries/device.js"
-import { FaGithub } from 'react-icons/fa';
-import { FaMedium } from 'react-icons/fa';
-import { FaTwitter } from 'react-icons/fa';
-
-
-
+import { device } from "../mediaqueries/device.js"
+import { FaGithub } from "react-icons/fa"
+import { FaMedium } from "react-icons/fa"
+import { FaTwitter } from "react-icons/fa"
 
 const ProjectCardWrapper = styled.section`
-grid-column: 1 / -1;
-display: grid;
-grid-template-columns: minmax(300px, 1024px);
-justify-items: center;
-justify-content: space-around;
-margin:   0rem 1rem 0rem 1rem;
+  grid-column: 1 / -1;
+  display: grid;
+  grid-template-columns: minmax(300px, 1024px);
+  justify-items: center;
+  justify-content: space-around;
+  margin: 0rem 1rem 0rem 1rem;
 
-@media ${device.mobileL} {
-  grid-gap: 1rem;
-}
+  @media ${device.mobileL} {
+    grid-gap: 1rem;
+  }
 `
 
-
-
 const ProjectCard = styled.div`
-display: ${props => props.hide ? 'none' : 'flex'};
-width: 100%;
-max-width: 1024px;
-flex-direction: column;
-align-items: flex-start;
-justify-content: space-between;
-background: hsl(210, 33%, 98%);
-border: 1px solid hsla(240, 10%, 90%, 1);
-border-top: 0px;
-padding: 1rem;
-
-@media ${device.tablet} {
+  display: ${props => (props.hide ? "none" : "flex")};
+  width: 100%;
+  max-width: 1024px;
   flex-direction: column;
-  justify-content: flex-start;
-}
+  align-items: flex-start;
+  justify-content: space-between;
+  background: hsl(210, 33%, 98%);
+  border: 1px solid hsla(240, 10%, 90%, 1);
+  border-top: 0px;
+  padding: 1rem;
 
-@media ${device.mobileL} {
-  max-width: 280px;
-  border-radius: 4px;
-  padding-bottom: 0;
-}
+  @media ${device.tablet} {
+    flex-direction: column;
+    justify-content: flex-start;
+  }
 
-:first-child {
-  border-top: 1px solid hsla(240, 10%, 90%, 1);
-  border-top-left-radius: 8px;
-  border-top-right-radius: 8px;
-}
+  @media ${device.mobileL} {
+    max-width: 280px;
+    border-radius: 4px;
+    padding-bottom: 0;
+  }
 
-:last-child {
-  border-bottom-left-radius: 8px;
-  border-bottom-right-radius: 8px;
-}
+  :first-child {
+    border-top: 1px solid hsla(240, 10%, 90%, 1);
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+  }
+
+  :last-child {
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+  }
 `
 
 const ProjectTopContainer = styled.div`
-display: flex;
-flex-direction: row;
-width:100%;
-justify-content: space-between;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
 `
 
-
-
 const ProjectRow = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
-flex-wrap: wrap;
-padding: 1rem 0rem 0.5rem 0rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 1rem 0rem 0.5rem 0rem;
 
-@media ${device.mobileL} {
-  padding: 1rem 0rem 1rem 0rem;
-}
+  @media ${device.mobileL} {
+    padding: 1rem 0rem 1rem 0rem;
+  }
 `
 
 const ProjectTitle = styled.h2`
-color: var(--headline-color);
-margin:0;
-text-overflow: ellipsis;
-overflow: hidden;
-white-space: nowrap;
-transition: 0.15s ease-in-out;
+  color: var(--headline-color);
+  margin: 0;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  transition: 0.15s ease-in-out;
 
-:hover {
-  color: var(--highlight-color--makerteal);
-}
+  :hover {
+    color: var(--highlight-color--makerteal);
+  }
 
-span {
-  color: var(--highlight-color--makerteal);
-}
-
+  span {
+    color: var(--highlight-color--makerteal);
+  }
 `
 
-
 const ProjectLabel = styled.span`
-font-size: 1rem;
-margin:0 1rem;
-color: var(--body-color);
-text-transform: uppercase;
-text-align: left;
+  font-size: 1rem;
+  margin: 0 1rem;
+  color: var(--body-color);
+  text-transform: uppercase;
+  text-align: left;
 
+  :nth-child(1) {
+    margin-left: 0rem;
+  }
 
-:nth-child(1) {
-  margin-left: 0rem;
-}
-
-@media ${device.mobileL} {
-  margin: 0.5rem;
-}
+  @media ${device.mobileL} {
+    margin: 0.5rem;
+  }
 `
 
 const ProjectLabelAudited = styled(ProjectLabel)`
-padding: 0.25rem 0.5rem 0.25rem 0.5rem;
-background: linear-gradient(hsla(168,50%,90%,1),hsla(168,50%,86%,1));
-color: var(--highlight-color--makerteal);
-border-radius: 4px;
+  padding: 0.25rem 0.5rem 0.25rem 0.5rem;
+  background: linear-gradient(hsla(168, 50%, 90%, 1), hsla(168, 50%, 86%, 1));
+  color: var(--highlight-color--makerteal);
+  border-radius: 4px;
 `
 
 const ProjectBottomContainer = styled.div`
-display: flex;
-flex-direction: row;
-flex-wrap: nowrap;
-justify-content: space-between;
-width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  width: 100%;
 
-@media ${device.tablet} {
-  flex-wrap: wrap;
-}
+  @media ${device.tablet} {
+    flex-wrap: wrap;
+  }
 `
 
-
 const ProjectDescription = styled.p`
-color: var(--body-color);
-width: 75%;
-white-space: nowrap;
-text-overflow: ellipsis;
-max-height: 140px;
-overflow: hidden;
-padding: 1rem 1rem 1rem 0rem;
-transition: 0.1s ease-in-out;
+  color: var(--body-color);
+  width: 75%;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  max-height: 140px;
+  overflow: hidden;
+  padding: 1rem 1rem 1rem 0rem;
+  transition: 0.1s ease-in-out;
 
-:hover {
-  /* Enable this and overflowing project descriptions will be revealed on hover */
-  /* white-space: pre-wrap; */
-}
+  :hover {
+    /* Enable this and overflowing project descriptions will be revealed on hover */
+    /* white-space: pre-wrap; */
+  }
 
-@media ${device.tablet} {
-  white-space: normal;
-}
+  @media ${device.tablet} {
+    white-space: normal;
+  }
 `
 
 const ProjectLinkContainer = styled.div`
-display: flex;
-justify-items: center;
-align-items: center;
+  display: flex;
+  justify-items: center;
+  align-items: center;
 `
 
 const ProjectLink = styled.a`
-display: flex;
-align-items: center;
-text-decoration: none;
-font-size: 1.25rem;
-margin: 0rem 0.475rem;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  font-size: 1.25rem;
+  margin: 0rem 0.475rem;
 `
 
 const TitleLinkLearnMore = styled.a`
-align-self: center;
-background: none;
-text-decoration: none;
-transition: 0.15s ease-in-out;
+  align-self: center;
+  background: none;
+  text-decoration: none;
+  transition: 0.15s ease-in-out;
 
+  :hover {
+    color: var(--highlight-color--makerteal);
 
-:hover {
-  color: var(--highlight-color--makerteal);
-
-  IcoArrow {
-    transform:translate(4px);
+    IcoArrow {
+      transform: translate(4px);
+    }
   }
-}
 `
 
 const IcoArrow = styled.span`
@@ -193,51 +181,51 @@ transition: 0.15s ease-in-out;
 color: var(--highlight-color--makerteal);
 `
 
-export default (props) => {
+export default props => {
   return (
     <ProjectCardWrapper disp={props.disp}>
-      {
-        props.selectedProjects.map( (project, index) => (
-          <ProjectCard key={project.name} hide={index > props.projectsToShow - 1}>
-            <ProjectTopContainer>
-              <TitleLinkLearnMore href={project.learnMoreLink} target="_blank" rel="noopener noreferrer">
-                <ProjectTitle>{project.name} <IcoArrow>↗</IcoArrow></ProjectTitle>
-              </TitleLinkLearnMore>
-                <ProjectLinkContainer>
-                {
-                  project.github ?
-                  <ProjectLink href={project.github}>
+      {props.selectedProjects.map((project, index) => (
+        <ProjectCard key={project.name} hide={index > props.projectsToShow - 1}>
+          <ProjectTopContainer>
+            <TitleLinkLearnMore
+              href={project.learnMoreLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ProjectTitle>
+                {project.name} <IcoArrow>↗</IcoArrow>
+              </ProjectTitle>
+            </TitleLinkLearnMore>
+            <ProjectLinkContainer>
+              {project.github ? (
+                <ProjectLink href={project.github}>
                   <FaGithub className={projectListRowStyles.ico} />
-                  </ProjectLink>
-                  : null
-                }
-                {project.medium ?
-                  <ProjectLink href={project.medium}>
+                </ProjectLink>
+              ) : null}
+              {project.medium ? (
+                <ProjectLink href={project.medium}>
                   <FaMedium className={projectListRowStyles.ico} />
-                  </ProjectLink>
-                  : null
-                }
-                {project.twitter ?
-                  <ProjectLink href={project.twitter}>
+                </ProjectLink>
+              ) : null}
+              {project.twitter ? (
+                <ProjectLink href={project.twitter}>
                   <FaTwitter className={projectListRowStyles.ico} />
-                  </ProjectLink>
-                  : null
-                }
-                </ProjectLinkContainer>
-            </ProjectTopContainer>
-            <ProjectDescription>{project.description}</ProjectDescription>
-            <ProjectBottomContainer>
+                </ProjectLink>
+              ) : null}
+            </ProjectLinkContainer>
+          </ProjectTopContainer>
+          <ProjectDescription>{project.description}</ProjectDescription>
+          <ProjectBottomContainer>
             <ProjectRow>
               <ProjectLabel>{project.status}</ProjectLabel>
-              <ProjectLabel>{project.location}</ProjectLabel>
-              {
-                project.audited ? <ProjectLabelAudited>Audited</ProjectLabelAudited> : null
-              }
+              <ProjectLabel>{project.country}</ProjectLabel>
+              {project.audited ? (
+                <ProjectLabelAudited>Audited</ProjectLabelAudited>
+              ) : null}
             </ProjectRow>
-            </ProjectBottomContainer>
-          </ProjectCard>
-        ))
-      }
+          </ProjectBottomContainer>
+        </ProjectCard>
+      ))}
     </ProjectCardWrapper>
   )
-};
+}
