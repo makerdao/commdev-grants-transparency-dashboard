644c5b2e6a1e62b1b846bd16eac992b2e4799551
Isaac Pearl <pearl.isaac@gmail.com>
2020-04-22 18:45:27 -0400

    moved footer files into index/meetups pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,28 @@ import PrimaryStatProjectStatus from "@modules/Layouts/PrimaryStatProjectStatus"
 import PrimaryStatProjectCountries from "@modules/Layouts/PrimaryStatProjectCountries"
 import ProjectList from '@modules/Layouts/ProjectList'
 import Stat from "@modules/Stats"
-import GrantsFooter from "@modules/ui/GrantsFooter"
 
 import {
   IntroHeader,
   IntroHeaderHighlight,
   IntroContent,
 } from "@modules/ui/styles/Header.styles"
+
+import Label from "@modules/ui/Label"
+import {
+  SectionWrapperFooter,
+  FooterBackgroundTop,
+  FooterColumn,
+  FooterCard,
+  FooterCardTitle,
+  FooterCardDesc,
+  FooterCardListContainer,
+  FooterCardListHeader,
+  FooterCardListItem,
+  FooterCardListFooter,
+  FooterCardListItemLeft,
+  FooterLink,
+} from "@modules/ui/styles/Footer.styles"
 
 import { grantsData, doughnutData } from "@src/utils.js"
 
@@ -109,6 +124,99 @@ export default () => (
       </Stat.Row>
     </Stat.Section>
     <ProjectList/>
-    <GrantsFooter/>
+    
+    {/*FOOTER*/}
+    <SectionWrapperFooter name="Footer">
+      <FooterBackgroundTop />
+      <FooterColumn columnLeft>
+        <FooterCard>
+          <FooterCardListContainer noBackground>
+            <FooterCardListHeader columnLeft>
+              <FooterCardTitle columnLeft>How can I get a grant?</FooterCardTitle>
+              <FooterCardDesc>
+                Get up to speed on the Maker project & Dai stablecoin:
+              </FooterCardDesc>
+            </FooterCardListHeader>
+            <FooterLink
+              href="https://community-development.makerdao.com/grants"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FooterCardListItemLeft noBackground>
+                <Label
+                  colorMakerBlue="true"
+                  labelContent="Questions about the grants program?"
+                ></Label>
+                Visit the Maker Grants Program FAQ
+              </FooterCardListItemLeft>
+            </FooterLink>
+            <FooterLink
+              href="https://github.com/makerdao/awesome-makerdao"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FooterCardListItemLeft noBackground>
+                <Label colorMakerBlue="true" labelContent="New to Maker?"></Label>
+                View a list of educational resources.
+              </FooterCardListItemLeft>
+            </FooterLink>
+            <FooterLink
+              href="https://chat.makerdao.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FooterCardListItemLeft noBackground>
+                <Label
+                  colorMakerBlue="true"
+                  labelContent="Meet the community"
+                ></Label>
+                Join the official MakerDAO chat.
+              </FooterCardListItemLeft>
+            </FooterLink>
+            <FooterLink
+              href="https://github.com/makerdao/awesome-makerdao#use-your-dai"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FooterCardListItemLeft noBackground>
+                <Label
+                  colorMakerBlue="true"
+                  labelContent="Looking for inspiration?"
+                ></Label>
+                Check out these community projects
+              </FooterCardListItemLeft>
+            </FooterLink>
+          </FooterCardListContainer>
+        </FooterCard>
+      </FooterColumn>
+
+      <FooterColumn>
+        <FooterCard hero>
+          <FooterCardListHeader>
+            <FooterCardTitle>APPLY</FooterCardTitle>
+            <FooterCardDesc>Write a 1-2 page proposal, answering:</FooterCardDesc>
+          </FooterCardListHeader>
+          <FooterCardListItem>→ What does it do, exactly?</FooterCardListItem>
+          <FooterCardListItem>→ What problem does it solve?</FooterCardListItem>
+          <FooterCardListItem>→ Why will it be successful?</FooterCardListItem>
+          <FooterCardListItem>
+            → Approximately what will it cost?
+          </FooterCardListItem>
+          <FooterCardListFooter applyCardFooter>
+          ↗ Send it to{" "}
+          <FooterLink inlineLink href="mailto:grants@makerdao.com">
+            grants@makerdao.com
+          </FooterLink>
+        </FooterCardListFooter>
+      </FooterCard>
+      <FooterCard>
+        <FooterCardListContainer>
+          <FooterCardListItem noBackground>
+            <Label labelContent="Due to the large number of submissions, please expect a response within 2-3 weeks."></Label>
+          </FooterCardListItem>
+        </FooterCardListContainer>
+      </FooterCard>
+    </FooterColumn>
+  </SectionWrapperFooter>
   </>
 )
