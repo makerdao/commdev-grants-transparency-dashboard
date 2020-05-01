f04584e35fc93faa73704b14a2b08dbdbc8c0721
Alex Singh <alex@alexsingh.com>
2020-05-01 08:26:15 -0300

    Add active state to links

--- src/modules/ui/Header.js
+++ src/modules/ui/Header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Location } from "@reach/router"
 
 import {
   IntroWrapper,
@@ -12,13 +13,17 @@ export default () => (
   <IntroWrapper>
     <HeaderNav>
       <HeaderNavItem>
-        <HeaderNavLink to="/">Grants</HeaderNavLink>
+        <HeaderNavLink to="/" activeClassName="active">
+          Grants
+        </HeaderNavLink>
       </HeaderNavItem>
       <HeaderNavItem>
         <LogoMaker src={"makerlogo.svg"} alt="Logo"></LogoMaker>
       </HeaderNavItem>
       <HeaderNavItem>
-        <HeaderNavLink to="/meetups">Meetups</HeaderNavLink>
+        <HeaderNavLink to="/meetups" activeClassName="active">
+          Meetups
+        </HeaderNavLink>
       </HeaderNavItem>
     </HeaderNav>
   </IntroWrapper>
