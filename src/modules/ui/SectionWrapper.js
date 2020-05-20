8292c5b9fcf07da6abe2a57df3a3d646e6821440
Alex Singh <alex@alexsingh.com>
2020-05-20 12:45:57 -0300

    Update padding

--- src/modules/ui/SectionWrapper.js
+++ src/modules/ui/SectionWrapper.js
@@ -6,6 +6,6 @@ export const SectionWrapper = styled.section`
   grid-template-columns: repeat(auto-fit, 300px);
   grid-gap: 0rem;
   justify-content: space-around;
-  padding: 2rem 1rem 4rem 1rem;
+  padding: 2rem 0.25rem 4rem 0.25rem;
   background: ${props => props.backgroundColor};
 `
