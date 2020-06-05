3b1d4cf1b14af57d89474eb5364fcd71c79db9de
Alex Singh <alex@alexsingh.com>
2020-06-05 15:21:18 -0300

    linting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,9 @@
-import React from 'react'
-import {MakerThemeProvider} from '@src/styles/MakerThemeContext'
-import  PageLayout  from "@modules/Layouts/PageLayout"
+import React from "react"
+import { MakerThemeProvider } from "@src/styles/MakerThemeContext"
+import PageLayout from "@modules/Layouts/PageLayout"
 
-export const wrapRootElement = ({element}) => (
-    <MakerThemeProvider theme="default">
-        <PageLayout>
-            {element}
-        </PageLayout>
-    </MakerThemeProvider>
+export const wrapRootElement = ({ element }) => (
+  <MakerThemeProvider theme="default">
+    <PageLayout>{element}</PageLayout>
+  </MakerThemeProvider>
 )
