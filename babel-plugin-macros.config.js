6b8f0804d923bebf254971d6ee51224561eea84d
Réjon Taylor-Foster <rejon@maximumcrash.com>
2021-03-25 09:54:58 -0400

    Updates

--- babel-plugin-macros.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  tailwind: {
-    styled: "styled-components",
-    config: './tailwind.config.js',
-    format: 'auto'
-  }
-}
--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { MakerThemeProvider } from "@src/styles/MakerThemeContext"
-import PageLayout from "@modules/Layouts/PageLayout"
-
-export const wrapRootElement = ({ element }) => (
-  <MakerThemeProvider theme="default">
-    <PageLayout>{element}</PageLayout>
-  </MakerThemeProvider>
-)
